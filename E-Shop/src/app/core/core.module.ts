5209f5587721d0a5e9d927bd02d08c744b673db3
Maxim Kraychev <theneraxes@gmail.com>
2023-07-20 17:42:38 +0300

    add footer component

--- E-Shop/src/app/core/core.module.ts
+++ E-Shop/src/app/core/core.module.ts
@@ -1,8 +1,13 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule
   ]
