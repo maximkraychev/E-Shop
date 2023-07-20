82e5295aa2f4ddd4b6f1b700a0216215adb29c47
Maxim Kraychev <theneraxes@gmail.com>
2023-07-20 17:46:31 +0300

    export header and footer components

--- E-Shop/src/app/core/core.module.ts
+++ E-Shop/src/app/core/core.module.ts
@@ -10,6 +10,10 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent
   ]
 })
 export class CoreModule { 
