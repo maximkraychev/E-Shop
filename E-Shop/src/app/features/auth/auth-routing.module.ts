614ac3c59c5fb132c886d9673f5e0e9ba8982078
Maxim Kraychev <theneraxes@gmail.com>
2023-07-21 18:47:20 +0300

    add login component

--- E-Shop/src/app/features/auth/auth-routing.module.ts
+++ E-Shop/src/app/features/auth/auth-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthComponent } from './auth.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
-  { path: 'login', component: AuthComponent },
-  { path: 'register', component: AuthComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
 ];
 
 @NgModule({
