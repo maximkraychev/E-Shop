00892d46c3ed04c445af8b1738e015fb3e2862b7
Maxim Kraychev <theneraxes@gmail.com>
2023-07-21 18:43:40 +0300

    add auth module

--- E-Shop/src/app/app-routing.module.ts
+++ E-Shop/src/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
-  { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) }
+  { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) },
+  { path: 'auth', loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule) },
+  { path: 'profile', loadChildren: () => import('./features/user/user.module').then(m => m.UserModule) }
 ];
 
 @NgModule({
