fd149167fd339a5d6d5dcf5edabb991f5c2a62ab
Maxim Kraychev <theneraxes@gmail.com>
2023-07-21 21:58:19 +0300

    trying to fix the css

--- E-Shop/src/app/core/core.module.ts
+++ E-Shop/src/app/core/core.module.ts
@@ -2,6 +2,7 @@ import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { RouterLink } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -9,7 +10,8 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterLink
   ],
   exports: [
     HeaderComponent,
