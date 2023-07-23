ace902e35d136a09b03b4625adee0254c47387e7
Maxim Kraychev <theneraxes@gmail.com>
2023-07-23 23:50:54 +0300

    trying bootstrap

--- E-Shop/src/app/app-routing.module.ts
+++ E-Shop/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TestComponent } from './test/test.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) },
   { path: 'auth', loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule) },
-  { path: 'profile', loadChildren: () => import('./features/user/user.module').then(m => m.UserModule) }
+  { path: 'profile', loadChildren: () => import('./features/user/user.module').then(m => m.UserModule) },
+  {path: 'test', component: TestComponent}
 ];
 
 @NgModule({
