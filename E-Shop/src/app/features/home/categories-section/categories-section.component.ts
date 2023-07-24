8dc8ecb0bfaa775fdf0867b4897b4c253190003e
Maxim Kraychev <theneraxes@gmail.com>
2023-07-24 16:05:36 +0300

    add categories component

---
+++ E-Shop/src/app/features/home/categories-section/categories-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categories-section',
+  templateUrl: './categories-section.component.html',
+  styleUrls: ['./categories-section.component.css']
+})
+export class CategoriesSectionComponent {
+
+}
--- E-Shop/src/app/features/home/home.module.ts
+++ E-Shop/src/app/features/home/home.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { IntroComponent } from './intro-section/intro.component';
+import { CategoriesSectionComponent } from './categories-section/categories-section.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    IntroComponent,
+    CategoriesSectionComponent
   ],
   imports: [
     CommonModule,
