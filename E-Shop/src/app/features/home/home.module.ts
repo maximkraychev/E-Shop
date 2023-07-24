3b007c51d5680d569aef102bba1c38d6c0193099
Maxim Kraychev <theneraxes@gmail.com>
2023-07-24 18:16:02 +0300

    add best selling book component (in progress)

--- E-Shop/src/app/features/home/home.module.ts
+++ E-Shop/src/app/features/home/home.module.ts
@@ -5,13 +5,15 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { IntroComponent } from './intro-section/intro.component';
 import { CategoriesSectionComponent } from './categories-section/categories-section.component';
+import { BestSellingBooksComponent } from './best-selling-books/best-selling-books.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
     IntroComponent,
-    CategoriesSectionComponent
+    CategoriesSectionComponent,
+    BestSellingBooksComponent
   ],
   imports: [
     CommonModule,
