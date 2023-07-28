5c437dd07dd749c22dcf3de9b866da9425ffa17a
MaximKraychev <maxim.kraychev@gmail.com>
2023-07-28 22:46:04 +0300

    set up firebase

--- E-Shop/src/app/app.module.ts
+++ E-Shop/src/app/app.module.ts
@@ -5,6 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { TestComponent } from './test/test.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
+import { provideMessaging,getMessaging } from '@angular/fire/messaging';
+import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -14,7 +23,15 @@ import { TestComponent } from './test/test.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    provideRemoteConfig(() => getRemoteConfig()),
+    provideStorage(() => getStorage())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ E-Shop/src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'book-store-34ab8',
+    appId: '1:1009290495741:web:49146b03cdf6400a356094',
+    storageBucket: 'book-store-34ab8.appspot.com',
+    apiKey: 'AIzaSyAFtyGhYx0058BXHDwyo0zOJ4BnwmF7O64',
+    authDomain: 'book-store-34ab8.firebaseapp.com',
+    messagingSenderId: '1009290495741',
+  },
+};
