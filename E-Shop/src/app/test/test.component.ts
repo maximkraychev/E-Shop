42a8de7b78a0d840b23e4966cb3075f6bb03f067
MaximKraychev <maxim.kraychev@gmail.com>
2023-07-29 17:32:59 +0300

    testing CRUT operation with firebase

--- E-Shop/src/app/test/test.component.ts
+++ E-Shop/src/app/test/test.component.ts
@@ -1,10 +1,7 @@
 import { Component, inject } from '@angular/core';
-import { collection, getFirestore, getDocs, Firestore, collectionData } from '@angular/fire/firestore';
+import { collection, getFirestore, getDocs, Firestore, collectionData, doc, docData, addDoc, updateDoc, deleteDoc, setDoc } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
-// import { onValue } from '@firebase/database';
-// import { AngularFirestore } from '@angular/fire/compat/firestore';
-//import { getFirestore } from 'firebase/firestore'
 
 @Component({
   selector: 'app-test',
@@ -13,24 +10,120 @@ import { Observable } from 'rxjs';
 })
 export class TestComponent {
 
-  item$: Observable<any>; 
+  item$: Observable<any>;
   firestore: Firestore = inject(Firestore);
 
   constructor() {
-    const collectionRef = collection(this.firestore, 'test');
-    this.item$ = collectionData(collectionRef);
-    this.item$.subscribe({
-      next: (data) => {console.log(data)}
-    })
-  }
+    // -------------------------------GET the entire colection----------------------
+    // const collectionRef = collection(this.firestore, 'test');
+    // this.item$ = collectionData(collectionRef);
+    // this.item$.subscribe({
+    //   next: (data) => {console.log(data)}
+    // })
 
-  // ngOnInit(): void {
-  //   const db = getFirestore();
-  //   const colRef = collection(db, 'test');
-  //   getDocs(colRef)
-  //     .then((snapshot) => {
-  //       console.log(snapshot.docs);
-  //     })
-  // }
+    // ------------------------------GET a single document-----------------------------------------------
+    const documentRef = doc(this.firestore, 'test', '3qZ61lBEnOMsk0l65EZe');
+    this.item$ = docData(documentRef);
+    this.item$.subscribe({
+      next: (data) => { console.log(data) }
+    })
+
+    // ---------------------------- POST request using observables(the other CRUT operation should work the same)--------------------------------------------
+    // const dataToBeAdded = { name: 'Ivan' };
+    // const colectionRef = collection(this.firestore, 'test');
+    // const newItem$: Observable<any> = new Observable((observer) => {
+    //   addDoc(colectionRef, dataToBeAdded)
+    //     .then((newDocRef) => {
+    //       observer.next(newDocRef.id);
+    //       observer.complete();
+    //     })
+    //     .catch((error) => {
+    //       observer.error(error);
+    //     })
+    // })
+
+    // newItem$.subscribe({
+    //   next: (newDocumentId) => {
+    //     console.log('Document added with ID:', newDocumentId);
+    //   },
+    //   error: (error) => {
+    //     console.error('Error adding document:', error);
+    //   },
+    // });
+
+
+
+    //---------------------------- UPDATE the document with the new data--------------------
+    // const dataToUpdate = { name: 'Pehso' };
+    // const docRef = doc(this.firestore, 'test', '3qZ61lBEnOMsk0l65EZe');
+    // const updateItem$: Observable<void> = new Observable((observer) => {
+    //   updateDoc(docRef, dataToUpdate)
+    //     .then(() => {
+    //       observer.next();
+    //       observer.complete();
+    //     })
+    //     .catch((error) => {
+    //       observer.error(error);
+    //     });
+    // });
+
+    // updateItem$.subscribe({
+    //   next: () => {
+    //     console.log('Document updated successfully');
+    //   },
+    //   error: (error) => {
+    //     console.error('Error updating document:', error);
+    //   },
+    // });
+
+
+
+    // ------------------------------ PUT request so it will completely replace the entire document --------------------------
+    // const dataToUpdate = { name: 'Pehso' };
+    // const docRef = doc(this.firestore, 'test', '3qZ61lBEnOMsk0l65EZe');
+    // const updateItem$: Observable<void> = new Observable((observer) => {
+    //   setDoc(docRef, dataToUpdate)
+    //     .then(() => {
+    //       observer.next();
+    //       observer.complete();
+    //     })
+    //     .catch((error) => {
+    //       observer.error(error);
+    //     });
+    // });
+
+    // updateItem$.subscribe({
+    //   next: () => {
+    //     console.log('Document updated successfully');
+    //   },
+    //   error: (error) => {
+    //     console.error('Error updating document:', error);
+    //   },
+    // });
+
+
+
+    // -----------------------------Delete the document with the specified ID-----------------------
+    // const docRef = doc(this.firestore, 'test', 'kxtK5hDVakTGoP9vchSo');
+    // const deleteItem$: Observable<void> = new Observable((observer) => {
+    //   deleteDoc(docRef)
+    //     .then(() => {
+    //       observer.next();
+    //       observer.complete();
+    //     })
+    //     .catch((error) => {
+    //       observer.error(error);
+    //     });
+    // });
+
+    // deleteItem$.subscribe({
+    //   next: () => {
+    //     console.log('Document deleted successfully');
+    //   },
+    //   error: (error) => {
+    //     console.error('Error deleting document:', error);
+    //   },
+    // });
+  }
 }
 
