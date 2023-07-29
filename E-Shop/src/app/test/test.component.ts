9800a508680c6a085786422d4de42aece1533ca5
MaximKraychev <maxim.kraychev@gmail.com>
2023-07-29 07:33:58 +0300

    testing for get request

--- E-Shop/src/app/test/test.component.ts
+++ E-Shop/src/app/test/test.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Database, ref } from '@angular/fire/database';
+// import { onValue } from '@firebase/database';
+// import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css']
 })
-export class TestComponent {
+export class TestComponent implements OnInit {
 
+  constructor(private db: Database) {
+  }
+
+  // ngOnInit(): void {
+  //   const dbRef = ref(this.db, 'test');
+  //   console.log(dbRef);
+  //   onValue(dbRef, (snapshot) => {
+  //     console.log(snapshot.val());
+  //   })
+  // }
+
+  // ngOnInit(): void {
+  //   const dbColection = AngularFirestore
+  //   console.log(dbColection);
+    
+  // }
+  ngOnInit(): void {
+    
+  }
 }
