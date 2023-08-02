9bfc54d85e58a317bdebaa47b0bb1abe09dade21
MaximKraychev <maxim.kraychev@gmail.com>
2023-08-02 21:50:44 +0300

    remove code that was used for testing

--- BookShop/src/app/features/auth/login/login.component.ts
+++ BookShop/src/app/features/auth/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,20 +11,16 @@ import { AuthService } from '../services/auth.service';
 export class LoginComponent {
 
   @ViewChild('form') form!: NgForm;
-  test = this.authService.user$.subscribe({
-    next: (user) => {
-      if (user) {
-        console.log(user.uid);
-      } 
-    }
-  })
-  constructor(private authService: AuthService) { }
 
+  constructor(private authService: AuthService,private router: Router) { }
 
   loginHandler() {
-    const fields = this.form.value
+    const fields = this.form.value;
     this.authService.signIn(fields.email, fields.password)
-      .then((userCredential) => console.log(userCredential))
-      .catch((err) => alert(err));
+      .finally(() => {this.router.navigate(['/home'])})
+      .catch((err) => {
+        //TODO handle the error;
+        alert(err) 
+      });
   }
 }
