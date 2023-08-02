77ebb739bd0042ee7f92c5758843b9927979778e
MaximKraychev <maxim.kraychev@gmail.com>
2023-08-02 21:36:41 +0300

    add logic for nav bar

--- BookShop/src/app/core/header/header.component.ts
+++ BookShop/src/app/core/header/header.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
 
+  userState!: boolean;
+  private userStateSubscription!: Subscription;
+
+  constructor(private afAuth: AngularFireAuth) {}
+
+  ngOnInit(): void {
+    this.userStateSubscription = this.afAuth.authState.subscribe({
+      next: (user) => {
+        this.userState = !!user;
+      },
+      error: (error) => {
+        console.error(error);
+        this.userState = false;
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.userStateSubscription.unsubscribe();
+  }
 }
