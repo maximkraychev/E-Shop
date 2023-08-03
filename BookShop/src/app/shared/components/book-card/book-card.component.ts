ae39d5661519b2a46f3f8788e64cb56cf06e3cbd
MaximKraychev <maxim.kraychev@gmail.com>
2023-08-03 11:16:36 +0300

    add book-card component

---
+++ BookShop/src/app/shared/components/book-card/book-card.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-book-card',
+  templateUrl: './book-card.component.html',
+  styleUrls: ['./book-card.component.css']
+})
+export class BookCardComponent {
+
+  @Input('book') book!: any;
+   
+}
