39cdfa66e75e78b0249fd88ca3be4fb4f31673d6
MaximKraychev <maxim.kraychev@gmail.com>
2023-08-03 13:17:46 +0300

    add discount for book-card

--- BookShop/src/app/features/catalog/catalog/catalog.component.ts
+++ BookShop/src/app/features/catalog/catalog/catalog.component.ts
@@ -7,10 +7,10 @@ import { Component } from '@angular/core';
 })
 export class CatalogComponent {
   mockData = [
-    {title: 'Game Of Thrones', author: 'George R. R. Martin', price: '30', discountPrice: '20', img: '/assets/book1.jpg'},
-    {title: 'Harry Potter And The Order of the Phoenix', author: 'J.K.Rowling', price: '30', discountPrice: '20', img: '/assets/book2.jpg'},
-    {title: 'I Am Number Four The Power of Six', author: 'Pittacus Lore', price: '30', discountPrice: '20', img: '/assets/book3.jpg'},
-    {title: 'The Intelligent Investor', author: 'Benjamin Graham', price: '30', discountPrice: '20', img: '/assets/book4.jpg'},
-    {title: 'Elon Musk', author: 'Ashlee Vance', price: '30', discountPrice: '20', img: '/assets/book5.jpg'},
+    {title: 'Game Of Thrones', author: 'George R. R. Martin', price: 50, discount: '', img: '/assets/book1.jpg'},
+    {title: 'Harry Potter And The Order of the Phoenix', author: 'J.K.Rowling', price: 30, discount: '', img: '/assets/book2.jpg'},
+    {title: 'I Am Number Four The Power of Six', author: 'Pittacus Lore', price: 50, discount: 30, img: '/assets/book3.jpg'},
+    {title: 'The Intelligent Investor', author: 'Benjamin Graham', price: 30, discount: 20, img: '/assets/book4.jpg'},
+    {title: 'Elon Musk', author: 'Ashlee Vance', price: 30, discount: 20, img: '/assets/book5.jpg'},
   ]
 }
