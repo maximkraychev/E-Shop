c64c1929489dfbd756a1d957710db63a90e81b59
MaximKraychev <maxim.kraychev@gmail.com>
2023-08-03 16:45:34 +0300

    updated User interface

--- BookShop/src/app/core/interfaces/user.interface.ts
+++ BookShop/src/app/core/interfaces/user.interface.ts
@@ -1,11 +1,13 @@
 import { IBook } from "./book.interface";
 
 export interface IUser {
-    firstName: string;
-    lastName: string;
-    addres: string;
-    phoneNumber: string;
+    name: string;
+    surname: string;
     email: string;
+    city: string;
+    addres: string;
+    postcode: number;
+    phoneNumber: string;
     authId: string;
     wishlist: IBook[] | [];
 }
