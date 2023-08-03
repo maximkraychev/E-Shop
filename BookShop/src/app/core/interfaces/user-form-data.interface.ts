d3a90839309a7cf88177591c64777fb9a7cf9e5f
MaximKraychev <maxim.kraychev@gmail.com>
2023-08-04 00:20:19 +0300

    add interface for register form data

---
+++ BookShop/src/app/core/interfaces/user-form-data.interface.ts
@@ -0,0 +1,10 @@
+export interface IUserFormData {
+    city: string;
+    deliveryAddres: string;
+    email: string;
+    name: string;
+    phoneNumber: number;
+    postcode: number;
+    surname: string;
+    password: string;
+}
