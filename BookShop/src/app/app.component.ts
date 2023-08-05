25a1362b26f05b32679acc9adabea3c849d4b1ca
MaximKraychev <maxim.kraychev@gmail.com>
2023-08-05 12:34:21 +0300

    app comp-t will display the view after auth check

--- BookShop/src/app/app.component.ts
+++ BookShop/src/app/app.component.ts
@@ -1,27 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { UserStateService } from './core/services/user-state.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnDestroy {
+
   title = 'BookShop';
   isLoading = true;
-  constructor(public userState: UserStateService) {}
+  private userSub: Subscription;
 
-  ngOnInit(): void {
-    this.userState.user$.subscribe({
-      next: (user) => {
-        if(user != null) {
-          this.isLoading = false;
-        }
+  constructor(public userState: UserStateService) {
+
+    this.userSub = this.userState.user$.subscribe({
+      next: () => {
+        this.isLoading = false;
       },
       error: (err) => {
         console.error(err);
         this.isLoading = false;
       }
     });
+  }
+
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
   }
 }
