5ab95cfdf543203ad75b180c34d9f0fef1e775e7
MaximKraychev <maxim.kraychev@gmail.com>
2023-08-05 17:01:07 +0300

    change place of sub from construc to afterViewInit

--- BookShop/src/app/core/error-component/error-component.component.ts
+++ BookShop/src/app/core/error-component/error-component.component.ts
@@ -3,6 +3,9 @@ import { ErrorPopupService } from '../services/error-popup.service';
 import { Subscription } from 'rxjs';
 
 
+// Currently is working only with one erorr msg;
+// TODO make it works with an array of errors msg;
+
 @Component({
   selector: 'app-error-component',
   templateUrl: './error-component.component.html',
@@ -12,9 +15,13 @@ export class ErrorComponentComponent implements OnDestroy{
  
   @ViewChild('activateButton') activateModal!: ElementRef<HTMLDivElement>;
   errorText: string = '';
-  private errorSubscription: Subscription;
+  private errorSubscription!: Subscription;
 
-  constructor(private errorService: ErrorPopupService) {
+  constructor(private errorService: ErrorPopupService) {}
+
+  // After the view is initialized and we have access to @ViewChild elements;
+  // Subscribe to the error service so other component can emmit errors to this component;
+  ngAfterViewInit() {
     this.errorSubscription = this.errorService.errorMsg$.subscribe({
       next: (errorMsg) => {
         this.errorText = errorMsg;
@@ -27,6 +34,8 @@ export class ErrorComponentComponent implements OnDestroy{
     })
   }
 
+  // It click on the button and showing up the popup modal;
+  // TODO find a better way to implement it; 
   activate() {
     this.activateModal.nativeElement.click();
   }
