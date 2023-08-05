63b707cc1cd30bb7c4bab1e60c1832da8ae905d7
MaximKraychev <maxim.kraychev@gmail.com>
2023-08-05 22:56:43 +0300

    remove from import shareModule

--- BookShop/src/app/app.module.ts
+++ BookShop/src/app/app.module.ts
@@ -5,8 +5,7 @@ import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoreModule } from './core/core.module'
-import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
 import { ErrorComponentComponent } from './core/error-component/error-component.component';
 
 @NgModule({
@@ -18,8 +17,7 @@ import { ErrorComponentComponent } from './core/error-component/error-component.
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
-    CoreModule,
-    SharedModule
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
