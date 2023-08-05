00f59b12751fdb0c43ae27a2576577eb6169150f
MaximKraychev <maxim.kraychev@gmail.com>
2023-08-05 17:02:29 +0300

    add comments

--- BookShop/src/app/core/services/error-popup.service.ts
+++ BookShop/src/app/core/services/error-popup.service.ts
@@ -9,6 +9,7 @@ export class ErrorPopupService {
   private errorMsgSubject: Subject<string> = new Subject;
   errorMsg$: Observable<string> = this.errorMsgSubject.asObservable();
 
+  // With this method we can push err msg from other components to error component;
   pushErrorMsg(error: string) {
     this.errorMsgSubject.next(error);
   }
