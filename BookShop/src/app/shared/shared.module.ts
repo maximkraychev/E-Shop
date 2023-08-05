856a1a1b635314608aa9ed430552461103d76186
MaximKraychev <maxim.kraychev@gmail.com>
2023-08-05 22:59:48 +0300

    in shared impoprts all internal ng modules needed

--- BookShop/src/app/shared/shared.module.ts
+++ BookShop/src/app/shared/shared.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookCardComponent } from './components/book-card/book-card.component';
+import { TrimDirective } from './directives/trim-directive';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [
-    BookCardComponent
+    BookCardComponent,
+    TrimDirective
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    RouterModule
   ],
   exports: [
-    BookCardComponent
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    BookCardComponent,
+    TrimDirective
   ]
 })
 export class SharedModule { }
