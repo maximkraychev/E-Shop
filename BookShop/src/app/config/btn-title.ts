9fbc90f079181734b915dc5cc65f854c1c05fb87
MaximKraychev <maxim.kraychev@gmail.com>
2023-08-06 23:41:38 +0300

    add config file with titles of the btn in the app

---
+++ BookShop/src/app/config/btn-title.ts
@@ -0,0 +1,14 @@
+const EDIT_PROFILE = 'Edit Profile';
+const SAVE_PROFILE = 'Save Profile';
+const DELETE = 'Delete';
+const LOGIN = 'Sign in';
+const REGISTER = 'Sign up';
+
+export const BTN_TITLES = {
+    EDIT_PROFILE,
+    SAVE_PROFILE,
+    DELETE,
+    LOGIN,
+    REGISTER
+}
+
