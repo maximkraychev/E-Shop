4599d89bc7eefd38a8788ec56813a9e2b559bc23
MaximKraychev <maxim.kraychev@gmail.com>
2023-08-06 11:28:03 +0300

    trim directive switch > @HostListener to Renderer2

--- BookShop/src/app/shared/directives/trim-directive.ts
+++ BookShop/src/app/shared/directives/trim-directive.ts
@@ -1,44 +1,39 @@
-// import { Directive } from '@angular/core';
-// import { NG_VALIDATORS, Validator, AbstractControl, ValidationErrors } from '@angular/forms';
-// import { trimValidator } from '../validators/trim-validator';
-
-// @Directive({
-//   selector: '[trim]',
-//   providers: [
-//     {
-//       provide: NG_VALIDATORS,
-//       useExisting: TrimDirective,
-//       multi: true
-//     }
-//   ]
-// })
-// export class TrimDirective implements Validator {
-//   validate(control: AbstractControl): ValidationErrors | null {
-//     console.log(control, 'aosdfajsifohajfuihuih');
-    
-//     return trimValidator(control);
-//   }
-// }
-
-import { Directive, HostListener, ElementRef, Renderer2, Input } from '@angular/core';
+import { Directive, ElementRef, Renderer2, OnInit, OnDestroy } from '@angular/core';
 
 @Directive({
-  selector: '[trim]',
-  exportAs: 'trim'
+  selector: '[trim]'
 })
-export class TrimDirective {
+export class TrimDirective implements OnInit, OnDestroy {
 
-  constructor(private elementRef: ElementRef, private renderer: Renderer2) {
+  private inputSub!: () => void;
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
+
+  ngOnInit(): void {
+    this.inputSub = this.renderer.listen(this.elementRef.nativeElement, 'input', this.onInput.bind(this));
   }
 
-  @HostListener('blur', ['$event.target.value'])
-  onBlur(value: string): void {
-    console.log(value);
+  onInput(event: InputEvent): void {
+    const value = (event.target as HTMLInputElement).value;
     
-    const trimmedValue = value.trim();
-    console.log(trimmedValue);
-    
-    this.renderer.setProperty(this.elementRef.nativeElement, 'value', trimmedValue);
+    if (typeof value == 'string') {
+      const trimmedValue = value.trim();
+      this.renderer.setProperty(this.elementRef.nativeElement, 'value', trimmedValue);
+    }
   }
+
+  ngOnDestroy(): void {
+    this.inputSub();
+  }
+
+  // This is another way of doing it but it should be worse based on google search :)
+  
+  // @HostListener('input', ['$event.target.value'])
+  // onInput(value: string): void {
+  //   if (typeof value == 'string') {
+  //     const trimmedValue = value.trim();
+  //     this.renderer.setProperty(this.elementRef.nativeElement, 'value', trimmedValue);
+  //   }
+  // }
+
 }
 
