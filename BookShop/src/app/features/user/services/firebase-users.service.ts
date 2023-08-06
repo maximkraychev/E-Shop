b863c2fe2c04a25c1d293db0cafbdba3bc4aa436
MaximKraychev <maxim.kraychev@gmail.com>
2023-08-06 23:44:41 +0300

    add updateUserData method in firebase-user.service

--- BookShop/src/app/features/user/services/firebase-users.service.ts
+++ BookShop/src/app/features/user/services/firebase-users.service.ts
@@ -7,7 +7,9 @@ import { FirebaseCRUTService } from 'src/app/core/services/firebase-crut.service
 @Injectable()
 export class FirebaseUsersService {
 
-  constructor(private fireService: FirebaseCRUTService) { }
+  constructor(private fireService: FirebaseCRUTService<IUser>) { }
+
+  // All of the methods here are for user data in fireStore;
 
   addUserWithId(document: IUser, id: string): Promise<void> {
     return this.fireService.addDataWithId(document, COLLECTIONS.USERS, id);
@@ -15,5 +17,9 @@ export class FirebaseUsersService {
 
   getUserDataById(documentId: string): Observable<IUser | undefined> {
     return (this.fireService.getById(documentId, COLLECTIONS.USERS)) as Observable<IUser | undefined>; 
+  }
+
+  updateUserData(documentId: string, userData: IUser): Promise<void> {
+    return this.fireService.updateData(documentId, userData, COLLECTIONS.USERS);
   }
 }
