2f193ee64d6094600399ee9176ee9340c6d7c7f0
MaximKraychev <maxim.kraychev@gmail.com>
2023-08-06 23:42:19 +0300

    add new interface IUserFromProfile

---
+++ BookShop/src/app/core/interfaces/user-data-fromProfile.interface.ts
@@ -0,0 +1,9 @@
+export interface IUserFormProfile {
+    city: string;
+    deliveryAddres: string;
+    email: string;
+    name: string;
+    phoneNumber: string;
+    postcode: string;
+    surname: string;
+}
