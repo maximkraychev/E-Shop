549331c34fccfe66f6f9fb293f3072c8e9706dea
MaximKraychev <maxim.kraychev@gmail.com>
2023-08-06 13:53:29 +0300

    implement load button to login page

--- BookShop/src/app/features/user/login/login.component.ts
+++ BookShop/src/app/features/user/login/login.component.ts
@@ -12,14 +12,17 @@ import { ErrorPopupService } from 'src/app/core/services/error-popup.service';
 export class LoginComponent {
 
   @ViewChild('loginForm') form!: NgForm;
+  btnLoad: boolean = false;
 
   constructor(private authService: AuthService, private router: Router, private errorService: ErrorPopupService) { }
 
   loginHandler() {
+    this.btnLoad= true;
     const fields = this.form.value;
     this.authService.signIn(fields.email, fields.password)
       .then(() => {this.router.navigate(['/home'])})
       .catch((err) => {
+        this.btnLoad = false;
         this.errorService.pushErrorMsg(err.message);
       });
   }
