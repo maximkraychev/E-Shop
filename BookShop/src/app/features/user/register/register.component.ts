bd8f8333f21cd6546760bd62841e13900b406678
MaximKraychev <maxim.kraychev@gmail.com>
2023-08-06 13:53:50 +0300

    implement login-btn component to register page

--- BookShop/src/app/features/user/register/register.component.ts
+++ BookShop/src/app/features/user/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, inject } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
@@ -7,6 +7,7 @@ import { FirebaseUsersService } from '../services/firebase-users.service';
 import { IUser } from 'src/app/core/interfaces/user.interface';
 import { IUserFormData } from 'src/app/core/interfaces/user-form-data.interface';
 import { ROLES } from 'src/app/config/user-roles';
+import { ErrorPopupService } from 'src/app/core/services/error-popup.service';
 
 
 @Component({
@@ -17,46 +18,55 @@ import { ROLES } from 'src/app/config/user-roles';
 export class RegisterComponent {
 
   @ViewChild('registerForm') form!: NgForm;
+  btnLoad: boolean = false;
 
   constructor(
     private authService: AuthService, 
     private usersService: FirebaseUsersService,
     private router: Router,
+    private errorService: ErrorPopupService
     ) { }
 
   async registerHandler() {
+    // Change button to loading;
+    this.btnLoad = true;
+
+    // Take the values from the Form;
     const formData: IUserFormData = this.form.value;
-    console.log(formData);
     
-    
-    // try {
-    //   const userCredential = await this.authService.signUp(formData.email, formData.password);
-    //     // Sign-up successful
+    try {
+      const userCredential = await this.authService.signUp(formData.email, formData.passwordGroup.password);   //Sign-up successful
+      
+        // Copy only the required inputs data from regiser form;
+        // That way we are sure there are no additionally added fluids;
+        if (userCredential.user?.uid) {
+          const userData: IUser = {
+            city: formData.city,
+            deliveryAddres: formData.deliveryAddres,
+            email: formData.email,
+            name: formData.name,
+            phoneNumber: formData.phoneNumber,
+            postcode: formData.postcode,
+            surname: formData.surname,
+            authId: userCredential.user.uid,
+            wishlist: [],
+            role: ROLES.BASE_ROLE
+          }
 
-    //     if (userCredential.user?.uid) {
-    //       const userData: IUser = {
-    //         city: formData.city,
-    //         deliveryAddres: formData.deliveryAddres,
-    //         email: formData.email,
-    //         name: formData.name,
-    //         phoneNumber: formData.phoneNumber,
-    //         postcode: formData.postcode,
-    //         surname: formData.surname,
-    //         authId: userCredential.user.uid,
-    //         wishlist: [],
-    //         role: ROLES.BASE_ROLE
-    //       }
-
-    //       await this.usersService.addUserWithId(userData, userCredential.user.uid);
-    //       this.router.navigate(['/home']);
+          // Save the user data in firestore;
+          await this.usersService.addUserWithId(userData, userCredential.user.uid);
+          this.router.navigate(['/home']);
           
-    //     } else {
-    //       throw new Error('We didin\'t find id for that user.');
-    //     }
-    //   } catch(err) {
-    //     console.log(err);
-    //     console.error(err);
-    //   }
+        } else {
+          throw new Error('We didin\'t find id for that user.');
+        }
+
+      } catch(err) {
+        this.btnLoad = false;
+        const error: Error = err as Error;
+        this.errorService.pushErrorMsg(error.message);
+        console.error(err);
+      }
       
   }
 }
