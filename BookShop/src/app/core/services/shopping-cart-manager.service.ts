cab49159c53300c9e0ca3e95e4968d60c6e79c5e
MaximKraychev <maxim.kraychev@gmail.com>
2023-08-10 01:58:10 +0300

    add comments to shopping-cart-manager service

--- BookShop/src/app/core/services/shopping-cart-manager.service.ts
+++ BookShop/src/app/core/services/shopping-cart-manager.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { IBook } from '../interfaces/book.interface';
 import { IShoppingCartData } from '../interfaces/shopping-cart-data.interface';
+import { SESSION_STORAGE_KEYS } from 'src/app/config/session-storage-keys';
 
 @Injectable({
   providedIn: 'root'
@@ -9,21 +10,23 @@ export class ShopingCartManagerService {
 
   constructor() { }
 
+  // Add the item to the session storage in array;
   private addItem(item: IShoppingCartData): void {
 
-    const shoppingCart: string | null = sessionStorage.getItem('shopping-cart');
+    const shoppingCart: string | null = sessionStorage.getItem(SESSION_STORAGE_KEYS.SHOPPING_CART);
 
     if (shoppingCart == null) {
-      sessionStorage.setItem('shopping-cart', JSON.stringify([item]));
+      sessionStorage.setItem(SESSION_STORAGE_KEYS.SHOPPING_CART, JSON.stringify([item]));
     } else {
       const shoppingCartData: IShoppingCartData[] = JSON.parse(shoppingCart);
       shoppingCartData.push(item)
-      sessionStorage.setItem('shopping-cart', JSON.stringify(shoppingCartData));
+      sessionStorage.setItem(SESSION_STORAGE_KEYS.SHOPPING_CART, JSON.stringify(shoppingCartData));
     }
   }
 
+  // Get all stored items in session and transfrom it to js array;
   getShopCart(): null | IShoppingCartData[] {
-    const shoppingCart: string | null = sessionStorage.getItem('shopping-cart');
+    const shoppingCart: string | null = sessionStorage.getItem(SESSION_STORAGE_KEYS.SHOPPING_CART);
 
     if (shoppingCart == null) {
       return null;
@@ -32,14 +35,16 @@ export class ShopingCartManagerService {
     }
   }
 
-  transformAndAddToSession(item: IBook, id: string): void {
+  // It takes Ibook and book id and transfrom it structure;
+  transformAndAddToSession(item: IBook, id: string, quantity:number = 1): void {
     return this.addItem({
       title: item.title,
       author: item.author,
       price: item.price,
       discount: item.discount,
       image: item.image,
-      id: id
+      id: id,
+      quantity: quantity
     }
     )
   }
