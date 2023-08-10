d4d02ed00a10b5e75e91d76ffe921d36f636edbe
MaximKraychev <maxim.kraychev@gmail.com>
2023-08-10 13:59:42 +0300

    applay is user guard on /user/profile

--- BookShop/src/app/features/user/user-routing.module.ts
+++ BookShop/src/app/features/user/user-routing.module.ts
@@ -4,9 +4,10 @@ import { ProfileComponent } from './profile/profile.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { LogoutComponent } from './logout/logout.component';
+import { isUserGuard } from 'src/app/core/guards/is-user.guard';
 
 const routes: Routes = [
-  { path: 'profile', component: ProfileComponent },
+  { path: 'profile', canMatch: [isUserGuard], component: ProfileComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'logout', component: LogoutComponent },
