b5f130035873437e5842d0a542a5865211a8710f
MaximKraychev <maxim.kraychev@gmail.com>
2023-08-10 13:58:01 +0300

    add is user guard

---
+++ BookShop/src/app/core/guards/is-user.guard.ts
@@ -0,0 +1,25 @@
+import { inject } from '@angular/core';
+import { CanMatchFn, Router } from '@angular/router';
+import { UserStateService } from '../services/user-state.service';
+import { map} from 'rxjs';
+import firebase from 'firebase/compat/app';
+import { ErrorPopupService } from '../services/error-popup.service';
+
+export const isUserGuard: CanMatchFn = (route, segments) => {
+
+  const userService = inject(UserStateService);
+  const errorService = inject(ErrorPopupService);
+  const router = inject(Router);
+
+  return userService.user$.pipe(
+    map((userData: firebase.User | null) => {
+      if(userData) {
+        return true
+      } else {
+        errorService.pushErrorMsg('Only users have access to this page');
+        router.navigate(['/user/login']);
+        return false;
+      }
+    })
+  )
+};
