7e71e4bce37eba6baa91d45b5cf641e2c82b4580
MaximKraychev <maxim.kraychev@gmail.com>
2023-08-10 23:03:14 +0300

    user-state service now emit and user data

--- BookShop/src/app/core/header/header.component.ts
+++ BookShop/src/app/core/header/header.component.ts
@@ -1,11 +1,8 @@
 import { Component, OnDestroy} from '@angular/core';
-import { Subscription, take } from 'rxjs';
+import { Subscription } from 'rxjs';
 import { UserStateService } from '../services/user-state.service';
 import firebase from 'firebase/compat/app';
 import { ErrorPopupService } from '../services/error-popup.service';
-import { FirebaseCRUTService } from '../services/firebase-crut.service';
-import { IUser } from '../interfaces/user.interface';
-import { COLLECTIONS } from 'src/app/config/firebase-collections';
 import { ROLES } from 'src/app/config/user-roles';
 
 @Component({
@@ -18,19 +15,17 @@ export class HeaderComponent implements OnDestroy {
   userState!: firebase.User | null;
   isAdmin: boolean = false;
   private userStateSubscription!: Subscription;
+  private userDataSubscription!: Subscription;
 
   constructor(
-    public userStateServbice: UserStateService, 
+    public userStateService: UserStateService, 
     private errorService: ErrorPopupService,
-    private firebaseService: FirebaseCRUTService<IUser>
     ) {
 
-    this.userStateSubscription = this.userStateServbice.user$.subscribe({
+      // SUB for user state (is guest or user);
+    this.userStateSubscription = this.userStateService.user$.subscribe({
       next: (user) => {
         this.userState = user;
-        if(user) {
-          this.checkForAdminRole(user.uid);
-        }
       },
       error: (error) => {
         console.error(error);
@@ -38,18 +33,23 @@ export class HeaderComponent implements OnDestroy {
         this.userState = null;
       }
     })
-  }
 
-  checkForAdminRole(userId: string) {
-    this.firebaseService.getById(userId, COLLECTIONS.USERS).pipe(take(1)).subscribe({
-      next: (data) => {
-        this.isAdmin = data?.role == ROLES.ADMIN;
+    //  SUB for user data (is it admin);
+    this.userDataSubscription = this.userStateService.userData$.subscribe({
+      next: (userData) => {
+        this.isAdmin = userData?.role == ROLES.ADMIN;
+      },
+      error: (err) => {
+        this.isAdmin = false;
+        console.error(err);
+        errorService.pushErrorMsg(err.message);
       }
     })
   }
 
+
   ngOnDestroy(): void {
     this.userStateSubscription.unsubscribe();
-    sessionStorage.removeItem('user-state');
+    this.userDataSubscription.unsubscribe();
   }
 }
--- BookShop/src/app/core/services/user-state.service.ts
+++ BookShop/src/app/core/services/user-state.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { Observable, ReplaySubject } from 'rxjs';
+import { Observable, ReplaySubject, take } from 'rxjs';
 import firebase from 'firebase/compat/app';
+import { COLLECTIONS } from 'src/app/config/firebase-collections';
+import { ROLES } from 'src/app/config/user-roles';
+import { FirebaseCRUTService } from './firebase-crut.service';
+import { IUser } from '../interfaces/user.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,25 +13,47 @@ import firebase from 'firebase/compat/app';
 export class UserStateService {
 
   // Its not working with BehaviorSubject because of the initial value;
-  private userSubject$$ = new ReplaySubject<firebase.User | null>(1);
-  user$: Observable<firebase.User | null> = this.userSubject$$.asObservable();
 
-  constructor(private afAuth: AngularFireAuth) {
+  // For user status is it guest or user;
+  private userAuthSubject$$ = new ReplaySubject<firebase.User | null>(1);
+  user$: Observable<firebase.User | null> = this.userAuthSubject$$.asObservable();
+
+  // This is for the whole user data (can be cheked if is admin)
+  private userDataSubject$$ = new ReplaySubject<IUser | null>(1);
+  userData$: Observable<IUser | null> = this.userDataSubject$$.asObservable();
+
+  // This emit user status;
+  constructor(private afAuth: AngularFireAuth, private firebaseService: FirebaseCRUTService<IUser>) {
     this.afAuth.authState.subscribe({
       next: (user) => {
-        this.userSubject$$.next(user);
+        this.userAuthSubject$$.next(user);
+        this.loadUserData(user?.uid);
       },
       error: (err) => {
-
-        //  TODO find a way to display the error component with error msg;
-        //  Righ now the error is passes before the error component is initialized and error msg won't show up;
-
-        //this.errorService.pushErrorMsg('Authentication Error: Unable to retrieve user information. Please try again later.');
-
         alert('Authentication Error: Unable to retrieve user information. Please try again later.');
         console.error(err);
-        this.userSubject$$.error(null);
+        this.userAuthSubject$$.error(null);
       }
     });
+  }
+
+  
+  // This will emit user data;
+  private loadUserData(userId: string | undefined) {
+    if(userId == undefined) {
+      this.userDataSubject$$.next(null);
+      return;
+    }
+
+    this.firebaseService.getById(userId, COLLECTIONS.USERS).pipe(take(1)).subscribe({
+      next: (data) => {
+        if(data == undefined) {
+          this.userDataSubject$$.next(null);
+          return
+        }
+
+        this.userDataSubject$$.next(data);
+      }
+    })
   }
 }
