7998f63f539ce609d2c7e7c72ae29fad397c7b7b
MaximKraychev <maxim.kraychev@gmail.com>
2023-08-10 22:35:04 +0300

    add nav button for admin panel and logic for view

--- BookShop/src/app/core/header/header.component.ts
+++ BookShop/src/app/core/header/header.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Component, OnDestroy} from '@angular/core';
+import { Subscription, take } from 'rxjs';
 import { UserStateService } from '../services/user-state.service';
 import firebase from 'firebase/compat/app';
+import { ErrorPopupService } from '../services/error-popup.service';
+import { FirebaseCRUTService } from '../services/firebase-crut.service';
+import { IUser } from '../interfaces/user.interface';
+import { COLLECTIONS } from 'src/app/config/firebase-collections';
+import { ROLES } from 'src/app/config/user-roles';
 
 @Component({
   selector: 'app-header',
@@ -11,19 +16,36 @@ import firebase from 'firebase/compat/app';
 export class HeaderComponent implements OnDestroy {
 
   userState!: firebase.User | null;
+  isAdmin: boolean = false;
   private userStateSubscription!: Subscription;
 
-  constructor(public userStateServbice: UserStateService) {
+  constructor(
+    public userStateServbice: UserStateService, 
+    private errorService: ErrorPopupService,
+    private firebaseService: FirebaseCRUTService<IUser>
+    ) {
+
     this.userStateSubscription = this.userStateServbice.user$.subscribe({
       next: (user) => {
         this.userState = user;
+        if(user) {
+          this.checkForAdminRole(user.uid);
+        }
       },
       error: (error) => {
         console.error(error);
+        this.errorService.pushErrorMsg(error.message);
         this.userState = null;
       }
     })
-    
+  }
+
+  checkForAdminRole(userId: string) {
+    this.firebaseService.getById(userId, COLLECTIONS.USERS).pipe(take(1)).subscribe({
+      next: (data) => {
+        this.isAdmin = data?.role == ROLES.ADMIN;
+      }
+    })
   }
 
   ngOnDestroy(): void {
