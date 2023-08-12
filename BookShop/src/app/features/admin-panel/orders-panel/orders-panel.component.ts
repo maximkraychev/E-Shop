5bfb85c04b0ccd4e6b8c4323084b79493322686c
MaximKraychev <maxim.kraychev@gmail.com>
2023-08-12 17:43:45 +0300

    add logic for orders component

--- BookShop/src/app/features/admin-panel/orders-panel/orders-panel.component.ts
+++ BookShop/src/app/features/admin-panel/orders-panel/orders-panel.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { OrdersService } from './orders.service';
+import { IOrderWithId } from 'src/app/core/interfaces/order.interface';
+import { ErrorPopupService } from 'src/app/core/services/error-popup.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-orders-panel',
   templateUrl: './orders-panel.component.html',
-  styleUrls: ['./orders-panel.component.css']
+  styleUrls: ['./orders-panel.component.css'],
+  providers: [
+    OrdersService
+  ]
 })
-export class OrdersPanelComponent {
+export class OrdersPanelComponent implements OnInit, OnDestroy {
+
+  orders: IOrderWithId[] = [];
+  btnsLoadStatus: boolean = false;
+
+  private ordersSub!: Subscription;
+
+  constructor(
+    private ordersService: OrdersService, 
+    private errorService: ErrorPopupService
+    ){}
+
+
+  //  On init load all orders;
+  ngOnInit(): void {
+    this.ordersSub = this.ordersService.getOrders().subscribe({
+      next: (ordersData) => {
+        this.orders = ordersData;
+      },
+      error: (err) => {
+        console.error(err);
+        this.errorService.pushErrorMsg(err.message);
+      }
+    })
+  }
+
+  //  On click delete that order;
+  onClickDone(orderId: string) {
+    this.btnsLoadStatus = true;
+    this.ordersService.deleteOrder(orderId)
+      .then(() => {this.btnsLoadStatus = false})
+      .catch((err) => {
+        console.error(err);
+        this.errorService.pushErrorMsg(err.message);
+        this.btnsLoadStatus = true;
+      })
+  }
+
+  ngOnDestroy(): void {
+    this.ordersSub.unsubscribe();
+  }
 
 }
--- BookShop/src/app/features/admin-panel/orders-panel/orders-panel.module.ts
+++ BookShop/src/app/features/admin-panel/orders-panel/orders-panel.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { OrdersPanelRoutingModule } from './order-panel.routing-module';
 import { OrdersPanelComponent } from './orders-panel.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [
     OrdersPanelComponent
   ],
   imports: [
-    CommonModule,
+    SharedModule,
     OrdersPanelRoutingModule
   ]
 })
