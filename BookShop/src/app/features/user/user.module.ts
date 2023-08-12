efdb01b84c2d42b763c42ebc126e1f7582903b8c
MaximKraychev <maxim.kraychev@gmail.com>
2023-08-12 14:37:44 +0300

    remove unused imports

--- BookShop/src/app/features/user/user.module.ts
+++ BookShop/src/app/features/user/user.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import { ProfileComponent } from './profile/profile.component';
-import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { LogoutComponent } from './logout/logout.component';
