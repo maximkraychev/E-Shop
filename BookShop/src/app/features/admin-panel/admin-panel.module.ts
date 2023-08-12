fe86491995692d08e0ef387e5b88545cbf259398
MaximKraychev <maxim.kraychev@gmail.com>
2023-08-12 14:39:44 +0300

    removed wrongly imported imports

--- BookShop/src/app/features/admin-panel/admin-panel.module.ts
+++ BookShop/src/app/features/admin-panel/admin-panel.module.ts
@@ -2,18 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminPanelComponent } from './admin-panel.component';
 import { AdminPanelRoutingModule } from './admin-panel-routing.module';
-import { CreateBookComponent } from './create-book/create-book.component';
 import { AdminSidebarComponent } from './admin-sidebar/admin-sidebar.component';
-import { OrdersPanelComponent } from './orders-panel/orders-panel.component';
+
 
 
 
 @NgModule({
   declarations: [
     AdminPanelComponent,
-    CreateBookComponent,
     AdminSidebarComponent,
-    OrdersPanelComponent
   ],
   imports: [
     AdminPanelRoutingModule,
